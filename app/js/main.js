9fd44d3f544c6e6d551b7cba2e1f1a6ed4148514
Andrey Kogut <andreyko.ol@gmail.com>
2016-02-05 17:04:58 +0200

    ++1

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,12 @@
 var everywhereDrop = $('#everywhere');
 var scrollBut = $('#scrollTop');
+var sortBut = $('#arrowSort');
+
+var filter = $('.filer');
+var scroll = $(".setScroll");
+var fancyBox = $('.fancybox');
+var tabs = $('#tabs-block');
+
 
 $(document).mouseup(function (e){
 
@@ -28,26 +35,48 @@ scrollBut.bind('click',function(){
     $('body').animate({scrollTop:0}, 500);
 });
 
+sortBut.bind('click',function(){
+    $('body').animate({scrollTop: $('#sort').offset().top}, 500);
+});
+
+
 
 
 
 (function($){
+   if(filter.length > 0) {
+
+       filter.selectize({
+           create: true
+       });
+   }
+
     $(window).load(function(){
-        $(".setScroll").mCustomScrollbar({
-            scrollbarPosition : 'inside',
-            autoHideScrollbar : true,
-            autoDraggerLength : false
-        });
-    });
-    $(document).ready(function() {
-        $('.fancybox').fancybox();
+        if(scroll.length > 0) {
+        scroll.mCustomScrollbar({
+                scrollbarPosition: 'inside',
+                autoHideScrollbar: true,
+                autoDraggerLength: false
+            });
+        }
     });
 
-    $('#tabs-block').responsiveTabs({
-        scrollToAccordion: true,
-        rotate: false,
-        startCollapsed: 'accordion',
-        enabled : 0
+    $(document).ready(function() {
+
+        if(fancyBox.length > 0) {
+        fancyBox.fancybox();
+        }
     });
+
+    if(tabs.length > 0) {
+        tabs.responsiveTabs({
+            scrollToAccordion: true,
+            rotate: false,
+            animation: 'slide',
+            startCollapsed: 'accordion',
+            enabled: 0
+        });
+    }
+
 })(jQuery);
 
