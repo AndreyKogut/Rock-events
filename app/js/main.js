ebe9417ec094570636392fc452c78ac295d9f1d7
Andrey Kogut <andreyko.ol@gmail.com>
2016-02-08 01:38:34 +0200

    Header, footer,home filter reworked some media added

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,47 @@
 var everywhereDrop = $('#everywhere');
+var everythingDrop = $('#everything');
+var allTimeDrop = $('#allTime');
 var scrollBut = $('#scrollTop');
 var sortBut = $('#arrowSort');
-
+var userCab = $('#userBut');
 var filter = $('.filer');
-var scroll = $(".setScroll");
 var fancyBox = $('.fancybox');
 var tabs = $('#tabs-block');
 
 
-$(document).mouseup(function (e){
+
+var scrollInit = function(elem){
+    if(!elem.length > 0) {
+        return;
+    } else {
+        elem.mCustomScrollbar({
+            scrollbarPosition: 'inside',
+            autoHideScrollbar: true,
+            autoDraggerLength: false
+
+        });
+    }
+};
+
+
+
+
+
+userCab.bind('click touchstart', function(){
+
+    var  but = this;
+    if(!$(but).find('.user-cabinet__head').hasClass('active')) {
+        $(but).find('.user-cabinet__head').addClass('active');
+        $(but).find('.user-cabinet__menu').slideDown('slow');
+    } else {
+        $(but).find('.user-cabinet__menu').slideUp(function(){
+            $(but).find('.user-cabinet__head').removeClass('active');
+        });
+    }
+
+});
+
+$(document).bind('mouseup touchend resize',function (e){
 
     if (everywhereDrop.is(e.target)) {
 
@@ -21,10 +54,37 @@ $(document).mouseup(function (e){
     } else {
         everywhereDrop.removeClass('active');
     }
+
+    if($(window).width()<960) {
+        if (everythingDrop.is(e.target)) {
+
+            everythingDrop.toggleClass('active');
+        } else if (everythingDrop.has(e.target).length !== 0) {
+            if ((e.target.nodeName.toLowerCase() == 'label') || $(e.target).hasClass('l-sort__header')) {
+                everythingDrop.find('.l-sort__header').html(e.target.innerHTML);
+                everythingDrop.toggleClass('active');
+            }
+        } else {
+            everythingDrop.removeClass('active');
+        }
+    }
+    if($(window).width()<600) {
+        if (allTimeDrop.is(e.target)) {
+
+            allTimeDrop.toggleClass('active');
+        } else if (allTimeDrop.has(e.target).length !== 0) {
+            if ((e.target.nodeName.toLowerCase() == 'label') || $(e.target).hasClass('l-sort__header')) {
+                allTimeDrop.find('.l-sort__header').html(e.target.innerHTML);
+                allTimeDrop.toggleClass('active');
+            }
+        } else {
+            allTimeDrop.removeClass('active');
+        }
+    }
 });
 
-$(window).scroll(function(){
-    if( $(this).scrollTop() > $(this).height()) {
+$(window).bind('scroll resize',function(){
+    if( ($(this).scrollTop() > $(this).height()) && ( $(this).width() > 960)) {
         scrollBut.show();
     } else {
         scrollBut.hide();
@@ -39,44 +99,56 @@ sortBut.bind('click',function(){
     $('body').animate({scrollTop: $('#sort').offset().top}, 500);
 });
 
+(function filInit(){
+    if(!filter.length > 0) {
+        return;
+    } else {
+        filter.selectize({
+            create: true
+        });
+    }
+})();
 
 
+$(window).bind('load resize', function(){
 
+   scrollInit(everywhereDrop.find('.l-sort__list'));
 
-(function($){
-   if(filter.length > 0) {
+    if($(window).width()<960) {
+        scrollInit(everythingDrop.find('.l-sort__list'));
+    } else {
+        everythingDrop.find('.l-sort__list').mCustomScrollbar('destroy');
+    }
 
-       filter.selectize({
-           create: true
-       });
-   }
+    if($(window).width()<650) {
+        scrollInit(allTimeDrop.find('.l-sort__list'));
+    } else {
+        allTimeDrop.find('.l-sort__list').mCustomScrollbar('destroy');
+    }
 
-    $(window).load(function(){
-        if(scroll.length > 0) {
-        scroll.mCustomScrollbar({
-                scrollbarPosition: 'inside',
-                autoHideScrollbar: true,
-                autoDraggerLength: false
-            });
-        }
-    });
+});
 
-    $(document).ready(function() {
-
-        if(fancyBox.length > 0) {
+(function fancyInit(){
+    if(!fancyBox.length > 0) {
+       return;
+    } else {
         fancyBox.fancybox();
-        }
-    });
+    }
+})();
 
-    if(tabs.length > 0) {
+(function tabInit(){
+    if(!tabs.length > 0) {
+        return;
+    } else {
         tabs.responsiveTabs({
             scrollToAccordion: true,
             rotate: false,
             animation: 'slide',
             startCollapsed: 'accordion',
             enabled: 0
-        });
+        })
     }
+})();
 
-})(jQuery);
+
 
